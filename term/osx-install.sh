97f55caf39550873fb604ff9ed1be3771d30e640
Ben Lowbridge <ben.lowbridge@mudbath.com.au>
2019-12-05 10:08:49 +1100

    Updates vimrc + Adds tmux config

---
+++ term/osx-install.sh
@@ -0,0 +1,42 @@
+# Install git and ruby and brew to get this up and running
+
+# Install workspace
+mkdir -p ~/Workspace/github.com/eastwood
+mkdir -p ~/Workspace/nib.com.au/cryan
+
+# Brew install
+brew install tmux
+brew install python3
+brew install ruby
+brew install neovim
+brew install nvm
+brew install fzf
+brew install zsh
+brew install reattach-to-user-namespace
+
+
+# Install config
+cd ~/Workspace/github.com/eastwood/
+git submodule update --init
+git submodule sync
+cd ~
+
+# Install tmux
+ln -s ~/Workspace/github.com/eastwood/config/term/tmux.conf ~/.tmux.conf
+ln -s ~/Workspace/github.com/eastwood/config/term ~/.config/term/
+
+# Install node and others
+nvm install 8
+npm install -g typescript
+npm install -g javascript-typescript-langserver
+
+# Install nvim
+mkdir -p ~/.config/nvim
+pip3 neovim
+ln -s ~/Workspace/github.com/eastwood/config/vim/init.vim ~/.config/nvim/init.vim
+ln -s ~/Workspace/github.com/eastwood/config/vim/vimrc ~/.vimrc
+
+# Install git bash
+ln -s ~/Workspace/github.com/eastwood/config/term/.zshrc ~/.zshrc
+
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
