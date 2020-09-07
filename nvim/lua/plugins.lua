86a960b91f8692f3a6dcf76dd6295c5c5f97aa50
Kylar101 <kylar101@protonmail.com>
2020-09-07 18:02:50 +1000

    Plugins

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -8,6 +8,12 @@ return require('packer').startup(
     use 'nvim-lua/plenary.nvim'
     use 'nvim-lua/telescope.nvim'
 
+    use { 'junegunn/fzf', run = './install --all' }
+    use { 'junegunn/fzf.vim' }
+    use { 'yuki-ycino/fzf-preview.vim', run = 'npm install' }
+
+    use 'neovim/nvim-lsp'
+
     use { 'scrooloose/nerdtree', opt = true, cmd = { 'NERDTreeToggle' } }
 
     use 'gruvbox-community/gruvbox'
